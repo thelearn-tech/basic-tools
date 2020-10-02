f7941a47ffd8b3fd840d84e376cd21f6d4b67645
Pritam Behera <69230350+thelearn-tech@users.noreply.github.com>
2020-10-02 15:03:59 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,48 +1,35 @@
 # basic-tools
 
 cd
-
-printf '\033[36m';echo script by THE_EAGLE
-
 echo
-
+printf '\033[36m';echo script by THE_EAGLE
+echo
+echo AKA Pritam Behera
+echo
+echo My YouTube It creates 19
+echo
 echo https://github.com/thelearn-tech/basic-tools
-
 sleep 4
-
 echo
-
-echo following script will install nano python figlet cowsay apache2 ngrok
-
+echo following script will install nano,python,figlet,cowsay,apache2,w3m,wget
 sleep 2
-
+echo
 pkg install nano python figlet cowsay apache2 w3m wget
-
 echo
-
 printf '\033[36m';echo installition complet
-
 echo
-
 echo ngrok will install now
-
 sleep 2
-
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
-
-unzip ngrok-stable-linux-arm.zip
-
-rm -rf ngrok-stable-linux-arm.zip
-
-sleep 2
-
-printf '\033[36m';echo script by THE_EAGLE
-
 echo
-
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip
+rm -rf ngrok-stable-linux-arm.zip
+sleep 2
+echo
+printf '\033[36m';echo script by THE_EAGLE
+echo
 echo https://github.com/thelearn-tech/basic-tools
-
-
+echo
 echo www.youtube.com/it creates 19
 echo
 echo https://m.youtube.com/channel/UCh2gmleSc9jwtQwqWZZsLdw
